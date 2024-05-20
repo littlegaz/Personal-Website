41f231322e2e31e42bca38fecca59da8a5852c68
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-20 16:05:25 +0100

    shapes project

---
+++ test and practise.js
@@ -0,0 +1,29 @@
+console.log(`Hello Apprille`);
+console.log(`i like pizza with pineapple`);
+
+//ctrl + f5 prints my console.logs
+
+// window.alert(`this is an alert`);
+// window.alert(`I like pineapple of pizza`);
+
+
+// let age = 37
+// let wage = 33233
+// let location = 'nottingham'
+// console.log(`you are ${age} years old and earn £${wage} a year and live in ${location}`);
+// let online = true
+// console.log(`you are online: ${online}`);
+// let forSale = false
+// console.log(`is this car for sale: ${forSale}`);
+
+// document.getElementById("p1").textContent = age
+// document.getElementById("p2").textContent = wage
+// document.getElementById("p3").textContent = location
+//here you can take a class and use the above js code to add the text instead of adding the text onto the html doc
+
+let students = 30;
+students = students +1;
+let classrooms = 16;
+classrooms = classrooms - 1;
+let extraStudents = students % 2 // this finds the remainder
+
